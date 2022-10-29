9031a6a4711fd491a9c5b9b406c552edd6f2a63c
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2022-10-29 10:55:41 +0100

    created defautl endpoint and added nodemon dep

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,13 @@ const express = require("express");
 
 const app = express();
 
+app.route("/").get((req, res) => {
+  res.json({
+    serverName: "culture-api",
+    connected: true,
+  });
+});
+
 app.listen(3000, () => {
   console.log(`Listening on port ${PORT}`);
 });
